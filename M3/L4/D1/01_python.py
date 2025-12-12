04879e9d77261101e8537de2aa012ae9b66e63bc
blandskron <bastianlandskronfreelancer@gmail.com>
2025-12-12 15:39:46 -0300

    update

--- M3/L4/D1/01_python.py
+++ M3/L4/D1/01_python.py
@@ -1,5 +1,22 @@
 # 1) while básico: contador ascendente
+"""
 contador = 1
 while contador <= 5:
     print("Contando:", contador)
     contador += 1
+"""
+from time import perf_counter
+
+# Inicio de medición
+inicio = perf_counter()
+
+# 1) while básico: contador ascendente
+contador = 1
+while contador <= 50000:
+    print("Contando:", contador)
+    contador += 1
+
+# Fin de medición
+fin = perf_counter()
+
+print(f"Tiempo de ejecución del while: {fin - inicio:.8f} segundos")
