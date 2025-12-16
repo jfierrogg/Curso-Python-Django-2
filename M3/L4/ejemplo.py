6859d4194fc774ac265666648f7d4df2c84d38ea
blandskron <bastianlandskronfreelancer@gmail.com>
2025-12-16 18:01:10 -0300

    update

---
+++ M3/L4/ejemplo.py
@@ -0,0 +1,19 @@
+numero = 160
+
+if numero >= 100:
+    print("El numero es mayor 0 igual 100")
+
+if numero >= 100:
+    print("El numero es mayor a 100")
+else:
+    print("El numero es menor a 100")
+
+if numero >= 100:
+    if numero <= 150:
+        print("numero mayor a  100 menor que 150")
+    else:
+        print("Numero mayor a 150")
+elif numero >= 50:
+    print("El numero es mayor a 50")
+else:
+    print("El numero es menor a 50")
